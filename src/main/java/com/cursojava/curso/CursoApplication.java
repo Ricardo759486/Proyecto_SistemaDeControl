8999a593801f6464269b22ff7af5ffd1b138a255
Mejooo08 <dfmejiar@unbosque.edu.co>
2022-05-28 22:32:03 -0500

    Envio Correo

--- src/main/java/com/cursojava/curso/CursoApplication.java
+++ src/main/java/com/cursojava/curso/CursoApplication.java
@@ -1,13 +1,23 @@
 package com.cursojava.curso;
 
+import com.cursojava.curso.service.impl.EnvioCorreoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class CursoApplication {
+	@Autowired
+	private EnvioCorreoImpl correoServ;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void envioCorreo() {
+		correoServ.enviarCorreo("dfmejiar@unbosque.edu.co", "Prueba Envio", "Bienvenido a la empresa");
+	}
 }
