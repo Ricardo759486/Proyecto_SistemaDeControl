926a5f91acc21aa836576c8850582c5df9661d31
Ricardo759486 <jrcuevas@unbosque.edu.co>
2022-06-04 15:40:36 -0500

    EDIT V3

--- src/main/java/com/cursojava/curso/controllers/AuditoriaRestController.java
+++ src/main/java/com/cursojava/curso/controllers/AuditoriaRestController.java
@@ -1,8 +1,11 @@
 package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.model.Auditoria;
+import com.cursojava.curso.model.Usuario;
 import com.cursojava.curso.service.AuditoriaServiceAPI;
+import com.cursojava.curso.service.UsuarioServiceAPI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,25 +18,32 @@ public class AuditoriaRestController {
 
     @Autowired
     private AuditoriaServiceAPI auditoriaServiceAPI;
+    @Autowired
+    private UsuarioServiceAPI usuarioServiceAPI;
 
     @GetMapping(value = "/getAll")
     public List<Auditoria> getAll(){
         return auditoriaServiceAPI.getAll();
     }
 
-    @PostMapping(value = "/saveAuditoria")
-    public ResponseEntity<Auditoria> save(@RequestBody Auditoria auditoria){
+    @PostMapping(value = "/saveAuditoria/{idUsuario}")
+    public ResponseEntity<Auditoria> save(@RequestBody Auditoria auditoria,
+                                          @PathVariable(value = "idUsuario") int idUsuario){
+        Usuario usuario = usuarioServiceAPI.get(idUsuario);
+        auditoria.setUsuario(usuario);
         Auditoria objeto = auditoriaServiceAPI.save(auditoria);
 
         return new ResponseEntity<Auditoria>(objeto, HttpStatus.OK);
     }
 
-    @PutMapping(value = "/updateAuditoria/{id}")
-    public ResponseEntity<Auditoria> update(@RequestBody Auditoria auditoria, @PathVariable(value = "id") int id){
-
+    @PutMapping(value = "/updateAuditoria/{id}/{idUsuario}")
+    public ResponseEntity<Auditoria> update(@RequestBody Auditoria auditoria,
+                                            @PathVariable(value = "id") int id,
+                                            @PathVariable(value = "idUsuario") int idUsuario){
+        Usuario usuario = usuarioServiceAPI.get(idUsuario);
         Auditoria objeto = auditoriaServiceAPI.get(id);
         if (objeto != null){
-            objeto.setUsuario(auditoria.getUsuario());
+            objeto.setUsuario(usuario);
             objeto.setFechaHora(auditoria.getFechaHora());
             objeto.setIpUsuario(auditoria.getIpUsuario());
             objeto.setOperacionCrud(auditoria.getOperacionCrud());
