2a22c237d99183a2c7c5b0ec50425ee60044eef1
Mejooo08 <dfmejiar@unbosque.edu.co>
2022-06-04 20:48:19 -0500

    Security

---
+++ src/main/java/com/cursojava/curso/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.cursojava.curso.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@AllArgsConstructor
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/Autenticacion/**").permitAll()
+                .antMatchers("/api2/**").permitAll()
+                .antMatchers("/api3/**").permitAll();
+    }
+}
