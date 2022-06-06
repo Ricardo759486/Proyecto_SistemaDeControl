e2b9661f1ce87f5b4e388674e3e57234615ab7a6
Ricardo759486 <jrcuevas@unbosque.edu.co>
2022-06-06 01:48:05 -0500

    r

--- src/main/resources/static/src/app/components/admin/admin.module.ts
+++ src/main/resources/static/src/app/components/admin/admin.module.ts
@@ -14,6 +14,7 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { ProveedorAdminRegisterComponent } from './proveedor-admin-register/proveedor-admin-register.component';
 import {FormsModule} from "@angular/forms";
+import { CuadrillasAdminComponent } from './cuadrillas-admin/cuadrillas-admin.component';
 
 
 @NgModule({
@@ -24,6 +25,7 @@ import {FormsModule} from "@angular/forms";
     HomeAdminComponent,
     AdminDashboardMainComponent,
     ProveedorAdminRegisterComponent,
+    CuadrillasAdminComponent,
 
   ],
   imports: [
