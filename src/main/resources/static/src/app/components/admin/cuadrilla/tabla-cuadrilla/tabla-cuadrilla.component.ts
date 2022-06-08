7e08cbcaa09500df4a3e1e5236c9c9b7d1a1c7b1
bmore <2003brayan24@gmail.com>
2022-06-08 14:00:24 -0500

    tablas

--- src/main/resources/static/src/app/components/admin/cuadrilla/tabla-cuadrilla/tabla-cuadrilla.component.ts
+++ src/main/resources/static/src/app/components/admin/cuadrilla/tabla-cuadrilla/tabla-cuadrilla.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Router} from "@angular/router";
 import {Cuadrilla} from "../../../../shared/models/Cuadrilla";
 import {CuadrillaAdminService} from "../../../../shared/services/admin/tabla_cuadrilla/cuadrilla-admin.service";
+import {Proveedor} from "../../../../shared/models/Proveedor";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
   selector: 'app-tabla-cuadrilla',
@@ -11,12 +15,30 @@ import {CuadrillaAdminService} from "../../../../shared/services/admin/tabla_cua
 export class TablaCuadrillaComponent implements OnInit {
 
   cuadrilla: Cuadrilla[] = [];
+  displayedColumns: string[] = ['ID', 'movil_Asociado','proveedor','turno_trabajo','zona', 'actions'];
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
   constructor(private tabla_admin_cuadrillascv: CuadrillaAdminService, private router:Router) { }
 
   ngOnInit(): void {
     this.tabla_admin_cuadrillascv.getCuadrilla().subscribe(data =>{
       this.cuadrilla = data;
+      this.dataSource.data = this.cuadrilla;
     });
+  }
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 
@@ -29,5 +51,8 @@ export class TablaCuadrillaComponent implements OnInit {
       this.cuadrilla = this.cuadrilla.filter(p => p !== cuadrilla);
       alert("Cuadrilla Eliminada");
     })
+  }
+  Agregar(){
+
   }
 }
